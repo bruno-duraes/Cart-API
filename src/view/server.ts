49d780b495b394bc4534e05b148b416732ed7d5b
bruno-duraes <brunodur4es@gmail.com>
2021-10-03 12:05:51 -0300

    update

--- src/view/server.ts
+++ src/view/server.ts
@@ -2,10 +2,12 @@ import Express from "express";
 import { route as productsRoute } from "./Products/products";
 import { route as categoriesRoute } from "./Categories/categories"
 import { route as ordersRoute } from "./Order/order";
+import cors from "cors"
 
 const apiPort = 3333
 const app = Express()
 app.use(Express.json())
+app.use(cors())
 app.use(productsRoute, categoriesRoute, ordersRoute)
 
 app.listen(apiPort, () => console.log("server running on port 3333"))
